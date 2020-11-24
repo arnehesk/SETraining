f6c3cca2e2db84d103e968290cf7afa98ea32ce5
Arneh Eskandari <arneh.eskandari@red-gate.com>
2020-11-24 13:13:21 -0800

    Init

---
+++ Tables/dbo.Contacts.sql
@@ -0,0 +1,16 @@
+CREATE TABLE [dbo].[Contacts]
+(
+[ID] [int] NOT NULL IDENTITY(1, 1),
+[Name] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[PhoneWork] [nvarchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[PhoneMobile] [nvarchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Address1] [nvarchar] (128) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Address2] [nvarchar] (128) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Address3] [nvarchar] (128) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[JoiningDate] [datetime] NULL CONSTRAINT [DF__Contacts__Joinin__3D5E1FD2] DEFAULT (getdate()),
+[Email] [nvarchar] (256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[TwitterID] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Contacts] ADD CONSTRAINT [PK_Contacts] PRIMARY KEY CLUSTERED  ([ID]) ON [PRIMARY]
+GO
---
+++ Tables/dbo.WidgetDescriptions.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [dbo].[WidgetDescriptions]
+(
+[WidgetID] [int] NOT NULL,
+[ShortDescription] [nvarchar] (2000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Description] [text] COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Picture] [image] NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[WidgetDescriptions] ADD CONSTRAINT [PK_WidgetDescriptions] PRIMARY KEY CLUSTERED  ([WidgetID]) ON [PRIMARY]
+GO
